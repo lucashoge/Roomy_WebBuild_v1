8342fe772670820bd6ab0a824d3203d240ac1aa8
lucashoge <lucashoge@gmx.de>
2022-07-25 17:07:37 +0200

    Styles_final_v1

---
+++ src/app/main-ui/keyframes.ts
@@ -0,0 +1,10 @@
+import { keyframes, style,animate } from "@angular/animations";
+
+export const match = [
+    style({ opacity: 0 }),
+    style({ transform: 'rotateY(90deg)' }),
+    style({ transform: 'rotateY(0eg)' }),
+    style({ opacity: 1}),
+  ]
+  
+  
